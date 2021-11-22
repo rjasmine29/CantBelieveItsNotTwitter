21d8b89cbd42872fd4dac5cd8aed8709c9cf6560
adyrmishi <angeladyrmishi@outlook.com>
2021-11-22 10:51:34 +0000

    first attempt at setter rfunctions

--- server/models/entries.js
+++ server/models/entries.js
@@ -7,6 +7,8 @@ class Entry {
         this.likes = data.likes;
         this.dislikes = data.dislikes;
         this.emoji = data.emoji;
+        this.gifUrl = data.gifUrl
+        this.reply = data.reply
     }
 
     static get all() {
@@ -28,6 +30,18 @@ class Entry {
         const newEntry = new Entry(entry);
         entries.push(newEntry);
         return newEntry;
+    }
+
+    set addGif(url) {
+        this.gifUrl = url;
+    }
+
+    set addReply(replyData) {
+        this.reply = replyData
+    }
+
+    set changeNumberOf(button) {
+        this[`${button}`] ++
     }
 }
 
